4415b282a6e7d4c34402c070a383f7898b1d6035
sanguneo <sknah0319@gmail.com>
2025-07-22 14:11:27 +0900

    사유 작성

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const schedules = sqliteTable('schedules', {
   id: text('id').primaryKey(), // uuid
-  userId: text('user_id').notNull(),
+  user: text('user').notNull(),
   date: text('date').notNull(), // YYYY-MM-DD
   type: text('type').notNull(), // 휴가, 외근 등
   reason: text('reason'), // 사유
